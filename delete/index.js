a1702932928a2cbbc00dd0206704777c7fcaa61e
Brandon L. Clark <support@themindspot.com>
2015-01-24 23:23:26 -0800

    Index to CRUD

--- delete/index.js
+++ delete/index.js
@@ -1,3 +1,9 @@
-/**
- * New node file
- */
+var sdl = require('./sdl');
+var iteration = require('./iteration');
+var task = require('./task');
+var story = require('./story');
+
+module.exports.sdl       = sdl;
+module.exports.iteration = iteration;
+module.exports.task      = task;
+module.exports.story     = story;
