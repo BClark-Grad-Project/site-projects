8f8ffac70956d04ba05cc950efe8cc02934cbe0c
Brandon L. Clark <support@themindspot.com>
2015-01-30 23:45:12 -0800

    stuatus updates for project and iteration done.

--- index.js
+++ index.js
@@ -12,40 +12,73 @@ var U = require('./update');
 var D = require('./delete');
 
 var updateCheck = function(){
-	var projectObj = {};
 	R({active:true}, function(err, project){
+	  var projectObj = {};
 		if(err){return -1;}
 		if(new Date(project.start).getTime() <= new Date().getTime()){
-			projectObj = project;
+			projectObj.id = project.id;
 			if(new Date(project.stop).getTime() >= new Date().getTime()){
-				project.status = 'Active';
+			  if(project.status === 'Pending'){
+				  projectObj.status = 'Active';
+				}
+				if(project.status === 'Complete'){
+				  projectObj.status = 'Active';
+				}
 			} else {
-				project.status = 'Complete';
+			  if(project.status === 'Complete'){
+				  projectObj.status = 'Pending';
+				}
+			  if(project.status === 'Active'){
+				  projectObj.status = 'Pending';
+				}
 			}
 			
-			delete projectObj.iteration;
-			delete projectObj.task;
-			delete projectObj.stories;
 			projectObj.iteration = [];
 			projectObj.task = [];
 			projectObj.stories = [];
-		}
+			console.log(project, projectObj);
+		} 
 		if(project.iteration){
 			for(var i in project.iteration){
-				if(new Date(project.iteration[i].start).getTime() <= new Date().getTime()){
-					if(new Date(project.iteration[i].stop).getTime() >= new Date().getTime()){
-						if(projectObj.iteration[i].status != 'Active'){
+				console.log(project.iteration[i].name, 'iteration status check');
+			  if(new Date(project.iteration[i].start).getTime() <= new Date().getTime()){
+				  console.log(project.iteration[i].name, 'past start');
+			    if(new Date(project.iteration[i].stop).getTime() >= new Date().getTime()){
+						if(project.iteration[i].status === 'Complete'){
+  				    console.log(project.iteration[i].name, 'make Active');
+							project.iteration[i].status = 'Active';
 							projectObj.iteration.push(project.iteration[i]);
-							projectObj.iteration[i].status = 'Active';
+						}
+						if(project.iteration[i].status === 'Pending'){
+  				    console.log(project.iteration[i].name, 'make Active');
+							project.iteration[i].status = 'Active';
+							projectObj.iteration.push(project.iteration[i]);
 						}
 					} else {
-						if(projectObj.iteration[i].status != 'Complete'){
+						if(project.iteration[i].status === 'Pending'){
+  				    console.log(project.iteration[i].name, 'make Complete');
+							project.iteration[i].status = 'Complete';
 							projectObj.iteration.push(project.iteration[i]);
-							projectObj.iteration[i].status = 'Complete';
 						}							
-					}
-				} 
-			}
+						if(project.iteration[i].status === 'Active'){
+  				    console.log(project.iteration[i].name, 'make Complete');
+							project.iteration[i].status = 'Complete';
+							projectObj.iteration.push(project.iteration[i]);
+						}
+				  }							
+			  } else {					  
+			    if(project.iteration[i].status === 'Active'){
+		        console.log(project.iteration[i].name, 'make Pending');
+  				  project.iteration[i].status = 'Pending';
+					  projectObj.iteration.push(project.iteration[i]);
+	  		  }							
+		    	if(project.iteration[i].status === 'Complete'){
+		        console.log(project.iteration[i].name, 'make Pending');
+			  	  project.iteration[i].status = 'Pending';
+				    projectObj.iteration.push(project.iteration[i]);
+			    }
+			  }	
+			} 
 		}
 		if(project.task){
 			for(var i in project.task){
@@ -115,5 +148,4 @@ module.exports.remove = function(Obj, cb){
 		return cb(null, data);
 	});
 };
-
 
