f51139f83956a924cbf509fb14ebb97a8caa825e
Brandon L. Clark <support@themindspot.com>
2015-02-09 07:41:26 -0800

    sort on iterations start date

--- read/iteration.js
+++ read/iteration.js
@@ -3,6 +3,7 @@ var Iteration = require('./../config').iteration;
 module.exports = function(Obj, cb){
 	Iteration
 		.find(Obj)
+		.sort('-start')  // mongoose has many methods of doing sort, look out for developer changes
 		.exec(function(err, results){
 			if(err){return cb(err, null);}
 			
